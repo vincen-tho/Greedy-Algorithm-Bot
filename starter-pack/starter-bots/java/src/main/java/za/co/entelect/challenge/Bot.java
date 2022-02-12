b99769ec13f70119144e1f968c30257eaf542be6
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-02-12 23:51:52 +0700

    add greedy_by_lane/greedy_by_avoiding_obstacle

--- starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -43,11 +43,6 @@ public class Bot {
         List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
         List<Object> nextBlocks = blocks.subList(0,1);
         
-        // List<Object> rightBlocks = getBlocksInRight(myCar.position.lane, myCar.position.lane, gameState);
-
-    
-        // List<Object> leftBlocks = getBlocksInLeft(myCar.position.lane, myCar.position.lane, gameState);
-
         //Fix first if too damaged to move
         if(myCar.damage == 5) {
             return FIX;
@@ -67,16 +62,45 @@ public class Bot {
 
         //Basic avoidance logic
         
-        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+        if (blocks.contains(Terrain.MUD) || blocks.contains(Terrain.WALL)) {
                 
-            if (myCar.position.lane != 3) {
+            if (myCar.position.lane != 4 && myCar.position.lane != 1) {
+                
+                List<Object> rightBlocks = getBlocksInFront(myCar.position.lane + 1, myCar.position.block, gameState);
+                List<Object> rightNextBlock = rightBlocks.subList(0, 1);
+
+                List<Object> leftBlocks = getBlocksInFront(myCar.position.lane - 1, myCar.position.block, gameState);
+                List<Object> leftNextBlock = leftBlocks.subList(0, 1);
+                
+                if (rightBlocks.contains(Terrain.MUD) || rightBlocks.contains(Terrain.WALL) || rightNextBlock.contains(Terrain.MUD) || rightNextBlock.contains(Terrain.WALL) && 
+                    !(leftBlocks.contains(Terrain.MUD) || leftBlocks.contains(Terrain.WALL) || leftNextBlock.contains(Terrain.MUD) || leftNextBlock.contains(Terrain.WALL))) {
+
+                    return TURN_LEFT;
+                
+                } else if (leftBlocks.contains(Terrain.MUD) || leftBlocks.contains(Terrain.WALL) || leftNextBlock.contains(Terrain.MUD) || leftNextBlock.contains(Terrain.WALL) && 
+                !(rightBlocks.contains(Terrain.MUD) || rightBlocks.contains(Terrain.WALL) || rightNextBlock.contains(Terrain.MUD) || rightNextBlock.contains(Terrain.WALL))) {
+
+                    return TURN_RIGHT;
+
+                } else {
+                    
+                    if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+               
+                        return LIZARD;
+                    
+                    }
+                
+                }
+
+            } else if (myCar.position.lane == 1) {
 
                 return TURN_RIGHT;
-            } else if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
-               
-                return LIZARD;
+
+            } else if (myCar.position.lane == 4) {
+                
+                return TURN_LEFT;
             
-            } 
+            }
 
         }
 
@@ -133,39 +157,6 @@ public class Bot {
         return blocks;
     }
 
-    // private List<Object> getBlocksInRight(int lane, int block, GameState gameState) {
-    //     List<Lane[]> map = gameState.lanes;
-    //     List<Object> blocks = new ArrayList<>();
-    //     int startBlock = map.get(0)[0].position.block;
-        
-    //     Lane[] laneList = map.get(lane);
-    //     for (int i = max(block - startBlock, 0); i <= block - startBlock + Bot.maxSpeed; i++) {
-    //         if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
-    //             break;
-    //         }
-
-    //         blocks.add(laneList[i].terrain);
-
-    //     }
-    //     return blocks;
-    // }
-
-    // private List<Object> getBlocksInLeft(int lane, int block, GameState gameState) {
-    //     List<Lane[]> map = gameState.lanes;
-    //     List<Object> blocks = new ArrayList<>();
-    //     int startBlock = map.get(0)[0].position.block;
-
-    //     Lane[] laneList = map.get(lane-2);
-    //     for (int i = max(block - startBlock, 0); i <= block - startBlock + Bot.maxSpeed; i++) {
-    //         if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
-    //             break;
-    //         }
-
-    //         blocks.add(laneList[i].terrain);
-
-    //     }
-    //     return blocks;
-    // }
 
 }
 
